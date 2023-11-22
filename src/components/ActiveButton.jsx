a925dbd757ccb9fb56f1b90200f810418467bd64
Rizky Cakra Mandala <rizky00164@gmail.com>
2023-11-22 15:13:46 +0700

    1.0

---
+++ src/components/ActiveButton.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ActiveButton({ id, onActive }) {
+    return (
+        <button className="note-item__archive-button" onClick={() => onActive(id)}>
+            Pindahkan
+        </button>
+    );
+}
+
+export default ActiveButton;
